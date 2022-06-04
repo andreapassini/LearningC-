4da7f4f514ba14d0cedbe6d9744f18b14f17980f
Andrea Passini <andreapa98@gmail.com>
2022-06-04 17:49:57 +0200

    aaaa

---
+++ MyFirstProject1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	std::cout << "Hello, from C++" << std::endl;
+
+	// Input from keyboard to not let the console close
+	std::cin.ignore();
+
+
+	return 0;
+}
