8e368daad94187140acf4db98a4f0a038acd830a
passinialessandra <passini.alessandraa@gmail.com>
2022-07-05 11:47:01 +0200

    New Project: Rubrica

---
+++ Project1/Project1/Rubrica.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main() {
+	int a;
+	int Numero1;
+	int Numero2;
+	int A[10];
+
+	while (a == 0)
+	{
+		std::cin >> a;
+	
+		if (a == 1)
+		{
+			std::cin >> Numero1;
+			A[0] = Numero1;
+		}
+
+		if (a == 2)
+		{
+			std::cin >> Numero2;
+			do
+			{
+				A[i] = 0
+			} while (A[i] != Numero2)
+		}
+
+		if (a == 3)
+		{
+			
+		}
+	}
+
+	
+}
