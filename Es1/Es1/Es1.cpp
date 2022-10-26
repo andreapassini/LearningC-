dd554b8fddf9b6a3bd3e8a1f5cb0a810d92462cb
Andrea Passini <andreapa98@gmail.com>
2022-10-26 21:47:29 +0200

    Rider

---
+++ Es1/Es1/Es1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello World\n";
+    std::cout << "Project finally working";
+    
+    return 0;
+}
---
+++ VarEx/VarEx/VarEx.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    cout << "Type your annual salary \n";
+    float annualSalary;
+    cin >> annualSalary;
+    
+    float monthSalary = annualSalary / 12;
+    cout << "Money per month: " << monthSalary;
+
+    cout << "In 10 years: " << annualSalary*10;
+    
+    return 0;
+}
