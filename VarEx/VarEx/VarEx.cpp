60e1f6220f677d5bdf6d4170acdbd919d37673ee
Andrea Passini <andreapa98@gmail.com>
2022-10-26 21:54:45 +0200

    dt

--- VarEx/VarEx/VarEx.cpp
+++ VarEx/VarEx/VarEx.cpp
@@ -9,8 +9,12 @@ int main(int argc, char* argv[])
     
     float monthSalary = annualSalary / 12;
     cout << "Money per month: " << monthSalary;
-
     cout << "In 10 years: " << annualSalary*10;
+
+    // single quotation marks for char
+    char character = 'a';
+
+    
     
     return 0;
 }
