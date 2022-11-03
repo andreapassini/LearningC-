7a449211d3669f75c78d368e4879130c00950fe7
Andrea Passini <andreapa98@gmail.com>
2022-11-03 19:24:25 +0100

    ascii output

--- ASCII/ASCII/ASCII.cpp
+++ ASCII/ASCII/ASCII.cpp
@@ -5,7 +5,9 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
+    // Output the ascii number of the letter
     cout << (int)'a' << endl;
+    cout << int('a') << endl;
     
     return 0;
 }
