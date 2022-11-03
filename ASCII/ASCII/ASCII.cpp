c0b579de5867483714ff5fcfc925cc658ac57044
Andrea Passini <andreapa98@gmail.com>
2022-11-03 19:25:35 +0100

    Ascii big letter

--- ASCII/ASCII/ASCII.cpp
+++ ASCII/ASCII/ASCII.cpp
@@ -8,6 +8,8 @@ int main(int argc, char* argv[])
     // Output the ascii number of the letter
     cout << (int)'a' << endl;
     cout << int('a') << endl;
+
+    cout << int('A') << endl;
     
     return 0;
 }
