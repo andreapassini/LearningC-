0f9753ed72e7e5bc2c6cf7c6586b4760c0ce96d6
Andrea Passini <andreapa98@gmail.com>
2022-11-03 19:59:29 +0100

    ASCII

--- ASCII/ASCII/ASCII.cpp
+++ ASCII/ASCII/ASCII.cpp
@@ -5,11 +5,18 @@ using namespace std;
 int main(int argc, char* argv[])
 {
     // Output the ascii number of the letter
-    cout << (int)'a' << endl;
-    cout << int('a') << endl;
+    // cout << (int)'a' << endl;
+    // cout << int('a') << endl;
+    //
+    // cout << int('A') << endl;
+    //
+    // cout << (char)65 << endl;
 
-    cout << int('A') << endl;
+    char c1, c2, c3, c4, c5;
+    cout << "Enter 5 letters" << endl;
+    cin >> c1 >> c2 >> c3 >> c4 >> c5;
 
-    cout << (char)65 << endl;
+    cout << "ASCII mes " << (int)c1 << " " << (int)c2 << " " << (int)c3 << " " << (int)c4 << " " << (int)c5;
+    
     return 0;
 }
