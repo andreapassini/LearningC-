75da2b08c31380224493673be3d53e515157f726
Andrea Passini <andreapa98@gmail.com>
2022-11-04 12:00:33 +0100

    only 2 values

--- Swap2Values/Swap2Values/Swap2Values.cpp
+++ Swap2Values/Swap2Values/Swap2Values.cpp
@@ -1,6 +1,30 @@
+#include <iostream>
+using namespace std;
 
 int main(int argc, char* argv[])
 {
+    int a = 6, b = 8;
+    cout << a << " " << b << endl;
+    
+    // With 3 values
+    int c = 0;
+    c = a;
+    a = b;
+    b = c;
+
+    cout << a << " " << b << endl;
+
+    system("cls");
+
+    a = 6;
+    b = 8;
+    cout << a << " " << b << endl;
+
+    a += b;
+    b = a - b;
+    a = a - b;
+
+    cout << a << " " << b << endl;
     
     return 0;
 }
