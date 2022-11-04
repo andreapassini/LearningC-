fdbc97123cebb66a4e1be5bfba810808787f3a0b
Andrea Passini <andreapa98@gmail.com>
2022-11-04 11:54:41 +0100

    Condition in cout

--- ArtiOperators/ArtiOperators/ArtiOperators.cpp
+++ ArtiOperators/ArtiOperators/ArtiOperators.cpp
@@ -3,7 +3,17 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
+    int counter = 0;
+    counter += 6;
 
+    cout << "Counter" << endl;
+    cout << counter++ << endl;
+
+    system("cls"); // Clean console
+
+    int a = 5, b = 5;
+    cout << "Condition" << endl;
+    cout << (a != b); // 1 if Diff, 0 otherwise
     
     return 0;
 }
