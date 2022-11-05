2f7425217c2e136eba5a508c0446101e31f7b86a
Andrea Passini <andreapa98@gmail.com>
2022-11-05 15:26:40 +0100

    Inverted Triangle

---
+++ DrawTriangle/DrawTriangle/DrawTriangle.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    // Draw a Rec Triangle
+    int base, height;
+    // cout << "Set Base of the triangle" << endl;
+    // cin >> base;
+    cout << "Set Height of the triangle" << endl;
+    cin >> height;
+
+    char symbol = '*';
+    
+    for(int i = 0; i < height; i++)
+    {
+        for (int j = height - i; j > 0; j--)
+        {
+            cout << setw(3) << symbol;
+        }
+
+        cout << endl;
+    }
+    
+    return 0;
+}
