9740ec222784e141d90b81e086c7063e1927020d
Andrea Passini <andreapa98@gmail.com>
2022-11-13 18:33:48 +0100

    Classes and inheritance

--- Objects/Objects/Objects.cpp
+++ Objects/Objects/Objects.cpp
@@ -4,45 +4,93 @@ using namespace std;
 
 class YouTubeChannel
 {
-public:
+private:
     string Name;
-    string OwnerName;
     int SubCount;
     list<string> PublishedVideoTitles;
 
+// Accessible to Child
+protected:
+    string OwnerName;
+    
+public:
     YouTubeChannel(string name, string owner)
     {
         Name = name;
         OwnerName = owner;
         SubCount = 0;
     }
-
     void GetInfo()
     {
         cout << "Name: " << Name << endl;
         cout << "Owner: " << OwnerName << endl;
         cout << "Subs: " << SubCount << endl;
         cout << "Videos: " << endl;
-        for(string videoTitle: PublishedVideoTitles)
+        for (string videoTitle : PublishedVideoTitles)
         {
             cout << "Video: " << videoTitle << endl;
         }
     }
-    
+    void Subscribe()
+    {
+        SubCount++;
+    }
+    void UnSub()
+    {
+        if (SubCount <= 0)
+            return;
+        SubCount--;
+    }
+    void PublishVideo(string title)
+    {
+        PublishedVideoTitles.push_back(title);
+    }
+};
+
+class CookingYoutubeChannel: public YouTubeChannel
+{
+public:
+    // Using the constructor of the father
+    CookingYoutubeChannel(string name, string ownerName):
+        YouTubeChannel(name, ownerName) {
+    }
+
+    void Practice() {
+        cout << OwnerName << " practicing " << endl;
+    }
+};
+
+class SingingYoutubeChannel: public YouTubeChannel
+{
+public:
+    // Using the constructor of the father
+    SingingYoutubeChannel(string name, string ownerName):
+        YouTubeChannel(name, ownerName) {
+    }
+
+    void Practice() {
+        cout << OwnerName << " LA LA LAAAAA! " << endl;
+    }
 };
 
 int main(int argc, char* argv[])
 {
-    YouTubeChannel ytChannel(
+    CookingYoutubeChannel cooking_youtube_channel(
         "AAAA",
         "Andrea"
     );
 
-    ytChannel.PublishedVideoTitles.push_back("1^ Video");
-    ytChannel.PublishedVideoTitles.push_back("2^ Video");
-    ytChannel.PublishedVideoTitles.push_back("3^ Video");
+    cooking_youtube_channel.Subscribe();
+    cooking_youtube_channel.Subscribe();
+    cooking_youtube_channel.Subscribe();
 
-    ytChannel.GetInfo();
+    cooking_youtube_channel.PublishVideo("1^");
+    cooking_youtube_channel.PublishVideo("2^");
+    cooking_youtube_channel.PublishVideo("3^");
+
+    cooking_youtube_channel.GetInfo();
+
+    cooking_youtube_channel.Practice();
 
     return 0;
 }
