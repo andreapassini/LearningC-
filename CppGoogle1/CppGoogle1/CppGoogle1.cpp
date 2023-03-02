9f7ba5dc813d56306be23f25f2ab2a02527f4d13
Andrea Passini <andreapa98@gmail.com>
2023-03-02 15:36:27 +0100

    input error handling

--- CppGoogle1/CppGoogle1/CppGoogle1.cpp
+++ CppGoogle1/CppGoogle1/CppGoogle1.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+constexpr auto max_size = std::numeric_limits<std::streamsize>::max();
+
 int main() {
     int input_var = 0;
     // Enter the do while loop and stay there until either
@@ -13,11 +15,11 @@ int main() {
         // cin returns false if an input operation fails, that is, if
         // something other than an int (the type of input_var) is entered.
         if (!(cin >> input_var)) {
-            cout << "You entered a non-numeric. Exiting..." << endl;
-            break;
-            // exit the do while loop
-        }
-        if (input_var != -1) {
+            cout << "You entered a non-numeric" << endl;
+            std::cin.ignore();
+            cin.clear();
+            std::cin.ignore(max_size, '\n'); // skip bad input
+        } else if (input_var != -1) {
             cout << "You entered " << input_var << endl;
         }
     } while (input_var != -1);
